a3e7748ba4a8d558f46775c01bfd7841ed208f5c
Xcedeveloper <xceldeveloper@gmail.com>
2022-06-20 01:03:24 +0100

    gbutton

--- src/components/XGroupButton/XGroupBtutton.stories.tsx
+++ src/components/XGroupButton/XGroupBtutton.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import XGroupButton from ".";
+import XGroupButton from "./XGroupButton";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
